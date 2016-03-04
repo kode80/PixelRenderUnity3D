52877f6d4918b71ca187ca750aefa4a6f7210df7
Ben Hopkins <ben@kode80.com>
2016-03-03 22:15:05 -0500

    Added GetR/G/B/A methods to Color32Util.

--- Assets/kode80/Utils/Color32Util.cs
+++ Assets/kode80/Utils/Color32Util.cs
@@ -23,10 +23,30 @@ namespace kode80.Utils
 	{
 		public static Color32 FromRGBAUint32( UInt32 color)
 		{
-			return new Color32( (byte) ((color >> 24) & 0xff),
-								(byte) ((color >> 16) & 0xff),
-								(byte) ((color >> 8) & 0xff),
-								(byte) (color & 0xff));
+			return new Color32( GetR( color),
+								GetG( color),
+								GetB( color),
+								GetA( color));
+		}
+
+		public static byte GetR( UInt32 color)
+		{
+			return (byte) ((color >> 24) & 0xff);
+		}
+
+		public static byte GetG( UInt32 color)
+		{
+			return (byte) ((color >> 16) & 0xff);
+		}
+
+		public static byte GetB( UInt32 color)
+		{
+			return (byte) ((color >> 8) & 0xff);
+		}
+
+		public static byte GetA( UInt32 color)
+		{
+			return (byte) (color & 0xff);
 		}
 	}
 }
